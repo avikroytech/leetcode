554437e3dff5859337014c3f6199d305c321ac59
avikroytech <68196967+avikroytech@users.noreply.github.com>
2024-11-10 21:58:00 -0800

    11/10 leetcode

---
+++ problems/countingBits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        bitCount = []
+
+        for i in range(n+1):
+            binary = bin(i)[2:]
+            bitCount.append(binary.count('1'))
+
+        return bitCount
